8f6ea17cb1860b06f42aa7e1a22d8809b98292eb
Rizki Laksana Putra <rizkilaksana007@gmail.com>
2025-03-21 05:00:41 +0700

    check point

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,16 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 const InputText = () => {
+  const [inputValue, setInputValue] = useState("");
+
+  const getValue = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(inputValue);
+  };
+
   return (
-    <form className="flex mb-[20px]">
+    <form className="flex mb-[20px]" onSubmit={handleSubmit}>
       <input
         type="text"
         placeholder="Add new task"
-        id="task-input"
-        className="flex-1 p-[10px] text-[16px] border-[#AB8BFF] border-2 border-r-0"
+        className="flex-1 p-[10px] text-[16px] border-[#AB8BFF] border-2 border-r-0 focus:outline-none focus:ring-0"
+        value={inputValue}
+        onChange={getValue}
         required
       />
-      <button className="py-[10px] px-[15px] bg-white border-[#AB8BFF] border-2 text-[16px] cursor-pointer hover:bg-[#AB8BFF] transition duration-300 ease-in-out">
+      <button
+        className="py-[10px] px-[15px] bg-white border-[#AB8BFF] border-2 text-[16px] cursor-pointer hover:bg-[#AB8BFF] transition duration-300 ease-in-out"
+        type="submit"
+      >
         Add
       </button>
     </form>
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -3,14 +3,12 @@ import React from "react";
 const ActivityList = () => {
   return (
     <>
-      <div>
-        <button>All List</button>
-        <button>Active</button>
-        <button>Completed</button>
+      <div className="flex justify-center items-center gap-4">
+        <button className="cursor-pointer rounded-sm border-1 border-[#AB8BFF] px-2 py-1 hover:bg-[#AB8BFF] transition duration-300 ease-in-out focus:bg-[#8355f7]">All List</button>
+        <button className="cursor-pointer rounded-sm border-1 border-[#AB8BFF] px-2 py-1 hover:bg-[#AB8BFF] transition duration-300 ease-in-out focus:bg-[#8355f7]">Active</button>
+        <button className="cursor-pointer rounded-sm border-1 border-[#AB8BFF] px-2 py-1 hover:bg-[#AB8BFF] transition duration-300 ease-in-out focus:bg-[#8355f7]">Completed</button>
       </div>
-      <ul className="list-none">
-        <li>alskdaw</li>
-      </ul>
+      <ul className="list-none"></ul>
     </>
   );
 };
